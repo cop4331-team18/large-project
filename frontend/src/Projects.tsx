ba44eb70047574a041d40661cabc0c09f61d2c1f
tiffhn <156131887+tiffhn@users.noreply.github.com>
2024-12-04 00:49:55 -0500

    created basic layout for projects page

--- frontend/src/Projects.tsx
+++ frontend/src/Projects.tsx
@@ -7,15 +7,42 @@ interface ProjectsProps {
 }
 const ProjectsPage: React.FC<ProjectsProps> = ({chatNotifications}: ProjectsProps) =>  {
   return (
-    <div>
       <div className="projects-page">
         {/* Tabs */}
         <Tabs currentTab="projects" chatNotifications={chatNotifications}/>
-        <header>
+        
+        <div className="projects-container"> 
           <h1>Project Management</h1>
-        </header>
+
+          <div className="section">
+            <div className="form-row">
+              <div className="form-group">
+                <label htmlFor="project-name">Name</label>
+                <input
+                  id="project-name"
+                  type="text"
+                  placeholder="Project Name"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="project-languages">Languages</label>
+                <input
+                  id="project-languages"
+                  type="text"
+                  placeholder="Project Languages"
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="project-description">Description</label>
+              <textarea
+                id="project-description"
+                placeholder="Project Description"
+              />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
   );
 };
 
