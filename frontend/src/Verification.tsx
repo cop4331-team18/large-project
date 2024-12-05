3252d64c2ae128a2a9927ec1466f6c41d35e1d9e
jstinhub <justin.sok.lee@gmail.com>
2024-12-04 20:51:24 -0500

    added verification page

---
+++ frontend/src/Verification.tsx
@@ -0,0 +1,17 @@
+import "./Verification.css";
+
+const Verification = () => {
+  return (
+    <div className="verification-page">
+      <div className="verification-container">
+        <h1 className="verification-title"> ❗ </h1>
+        <h1 className="verification-title">Email Verification Required</h1>
+        <p className="verification-message">
+          Please check your email and use the link to verify your account
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Verification;
